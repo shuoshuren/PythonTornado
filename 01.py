0b25b3bd13a43436488f22672ffa3edb2401b34a
shuoshuren <534361124@qq.com>
2018-09-20 22:22:14 +0800

    first commit

---
+++ 01.py
@@ -0,0 +1,19 @@
+# coding=utf-8
+
+import tornado.web
+import tornado.ioloop
+
+
+class IndexHandler(tornado.web.RequestHandler):
+	"""主页处理类"""
+	def  get(self):
+		"""get请求方式"""
+		self.write("hello tornado")
+
+		
+
+
+if __name__ == '__main__':
+	app = tornado.web.Application([("/",IndexHandler)])
+	app.listen(8000)
+	tornado.ioloop.IOLoop.current().start()
